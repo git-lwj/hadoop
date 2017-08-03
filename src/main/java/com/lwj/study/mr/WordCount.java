83efe410795bfd057dd217533ea0cff61d3e7a65
梁文剑 <liangwj9@ziroom.com>
2017-08-03 17:20:37 +0800

    WordCount

---
+++ src/main/java/com/lwj/study/mr/WordCount.java
@@ -0,0 +1,17 @@
+package com.lwj.study.mr;
+
+/**
+ * <p></p>
+ * <p>
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author liangwj9
+ * @version 1.0
+ * @since 1.0
+ */
+public class WordCount {
+}
